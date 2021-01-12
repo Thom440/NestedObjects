36581a9b982c88ef939e32167826b0a663e37b89
Thom Barrett <thom440@yahoo.com>
2021-01-12 12:45:10 -0800

    Created Instructor Class

---
+++ NestedObjects/Instructor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NestedObjects
+{
+    /// <summary>
+    /// Represents an individual instructor
+    /// </summary>
+    class Instructor
+    {
+        /// <summary>
+        /// Legal full name
+        /// </summary>
+        public string FullName { get; set; }
+
+        /// <summary>
+        /// The instructors work email address
+        /// </summary>
+        public string Email { get; set; }
+    }
+}
